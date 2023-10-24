fec564512b45fb9e0cf3cbec34ac4522ddd7e584
Sandaru Perera <sandaruxd99@gmail.com>
2023-10-24 19:22:47 +0530

    adding searchWeather data functionality

--- weather_forecast/src/components/current_weather/CurrentWeather.jsx
+++ weather_forecast/src/components/current_weather/CurrentWeather.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
-import LocationIcon from '../../assets/location.svg'
-import ClearImage from '../../assets/clear.png'
+import React, { useEffect, useState } from 'react';
+import LocationIcon from '../../assets/location.svg';
+import ClearImage from '../../assets/clear.png';
+import ThunderIcon from '../../assets/thunder_rain.png';
+import SnowIcon from '../../assets/snow.png';
+import Showericon from '../../assets/shower_rain.png';
+import ScatteredIcon from '../../assets/scattered_clouds.png';
+import RainIcon from '../../assets/rain.png';
+import MistIcon from '../../assets/mist.png';
+import FewCloudIcon from '../../assets/f_clouds.png';
+import BrokenCloudIcon from '../../assets/broken_clouds.png';
+
+
 import './CurrentWeather.css'
 
 const CurrentWeather = ({current}) => {
   if (!current) {
     return <div>Loading...</div>;
   }
+  const [weatherIcon, setWeatherIcon] = useState(ClearImage);
+  useEffect(()=>{
+    if(current.weather[0].icon==="01d" || current.weather[0].icon==="01n"){
+      setWeatherIcon(ClearImage);
+    }
+    else if(current.weather[0].icon==="02d" || current.weather[0].icon==="02n"){
+      setWeatherIcon(FewCloudIcon);
+    }
+    else if(current.weather[0].icon==="03d" || current.weather[0].icon==="03n"){
+      setWeatherIcon(ScatteredIcon);
+    }
+    else if(current.weather[0].icon==="04d" || current.weather[0].icon==="04n"){
+      setWeatherIcon(BrokenCloudIcon);
+    }
+    else if(current.weather[0].icon==="09d" || current.weather[0].icon==="09n"){
+      setWeatherIcon(Showericon);
+    }
+    else if(current.weather[0].icon==="10d" || current.weather[0].icon==="10n"){
+      setWeatherIcon(RainIcon);
+    }
+    else if(current.weather[0].icon==="11d" || current.weather[0].icon==="11n"){
+      setWeatherIcon(ThunderIcon);
+    }
+    else if(current.weather[0].icon==="13d" || current.weather[0].icon==="13n"){
+      setWeatherIcon(SnowIcon);
+    }
+    else if(current.weather[0].icon==="50d" || current.weather[0].icon==="50n"){
+      setWeatherIcon(MistIcon);
+    }
+  },[])
   
   return (
     <div className="container">
@@ -23,7 +63,7 @@ const CurrentWeather = ({current}) => {
             </div>
         </div>
         <div className="weather-image">
-            <img src={ClearImage} alt=''/>
+            <img src={weatherIcon} alt=''/>
         </div>
     </div>
   )
--- weather_forecast/src/components/search_weather/SearchWeather.jsx
+++ weather_forecast/src/components/search_weather/SearchWeather.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import './SearchWeather.css'
+import React from "react";
+import { useState } from "react";
+import "./SearchWeather.css";
+import { searchWeatherData } from '../../api/apiClient'
 
-const SearchWeather = () => {
+const SearchWeather = ({ onSearch }) => {
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [error, setError] = useState('');
+
+  const handleSearch = () => {
+    if (latitude.trim() === '' || longitude.trim() === '') {
+      setError('Please enter valid details.');
+    } else if (!/^-?\d+(\.\d+)?$/.test(latitude) || !/^-?\d+(\.\d+)?$/.test(longitude)) {
+      setError('Please enter valid numbers for latitude and longitude.');
+    } else {
+      setError('');
+      onSearch({ latitude, longitude });
+    }
+  };
   return (
-    <div className='main-conntainer'>
-     <div className="field" >
-            <p>Latitude</p>
-            <input type="text" id="username" name="username" placeholder='latitude'/>
-        </div>
-        <div className="field">
-            <p>Longitude</p>
-            <input type="password" id="password" name="password" placeholder='longitude'/>
-        </div>   
-        <button>Search</button>
+    <div className="main-conntainer">
+      <div className="field">
+        <p>Latitude</p>
+        <input
+          type="text"
+          id="latitude"
+          name="latitude"
+          placeholder="latitude"
+          value={latitude}
+          onChange={(e) => setLatitude(e.target.value)}
+        />
+      </div>
+      <div className="field">
+        <p>Longitude</p>
+        <input
+          type="text"
+          id="longitude"
+          name="longitude"
+          placeholder="longitude"
+          value={longitude}
+          onChange={(e) => setLongitude(e.target.value)}
+        />
+      </div>
+      <button onClick={handleSearch}>Search</button>
+      {error && <p className="error-message">{error}</p>}
     </div>
-  )
-}
+  );
+};
 
-export default SearchWeather
+export default SearchWeather;
