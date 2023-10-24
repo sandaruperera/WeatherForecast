05f24d4d370a87ad42557746e7acff93b834bf24
sandaruperera <sandaruxd99@gmail.com>
2023-10-24 21:00:16 +0530

    card components updated with fetched data

--- weather_forecast/src/components/simple_forecast/Card.jsx
+++ weather_forecast/src/components/simple_forecast/Card.jsx
@@ -1,15 +1,54 @@
-import React from "react";
+import React, { useState , useEffect} from "react";
 import "./Card.css";
 import ClearImage from "../../assets/clear.png";
+import ThunderIcon from '../../assets/thunder_rain.png';
+import SnowIcon from '../../assets/snow.png';
+import Showericon from '../../assets/shower_rain.png';
+import ScatteredIcon from '../../assets/scattered_clouds.png';
+import RainIcon from '../../assets/rain.png';
+import MistIcon from '../../assets/mist.png';
+import FewCloudIcon from '../../assets/f_clouds.png';
+import BrokenCloudIcon from '../../assets/broken_clouds.png';
 
-const Card = () => {
+const Card = ({date, temp, description,icon,humidity}) => {
+
+  const [weatherIcon, setWeatherIcon] = useState(ClearImage);
+  useEffect(()=>{
+    if(icon==="01d" || icon==="01n"){
+      setWeatherIcon(ClearImage);
+    }
+    else if(icon==="02d" ||icon==="02n"){
+      setWeatherIcon(FewCloudIcon);
+    }
+    else if(icon==="03d" || icon==="03n"){
+      setWeatherIcon(ScatteredIcon);
+    }
+    else if(icon==="04d" || icon==="04n"){
+      setWeatherIcon(BrokenCloudIcon);
+    }
+    else if(icon==="09d" || icon==="09n"){
+      setWeatherIcon(Showericon);
+    }
+    else if(icon==="10d" || icon==="10n"){
+      setWeatherIcon(RainIcon);
+    }
+    else if(icon==="11d" || icon==="11n"){
+      setWeatherIcon(ThunderIcon);
+    }
+    else if(icon==="13d" || icon==="13n"){
+      setWeatherIcon(SnowIcon);
+    }
+    else if(icon==="50d" ||icon==="50n"){
+      setWeatherIcon(MistIcon);
+    }
+  },[])
   return (
     <div className="card-container">
-      <p className="day">Mon</p>
-      <img src={ClearImage} alt="" />
-      <p className="card-temperature">30°C</p>
-      <p className="card-description">Clear Sky</p>
-      <p className="card-humidity">72%</p>
+      <p className="day">{date}</p>
+      <img src={weatherIcon} alt="" />
+      <p className="card-temperature">{temp} °C</p>
+      <p className="card-description">{description}</p>
+      <p className="card-humidity">Humidity: {humidity} %</p>
     </div>
   );
 };
--- weather_forecast/src/pages/home/Home.jsx
+++ weather_forecast/src/pages/home/Home.jsx
@@ -1,30 +1,59 @@
 import { useState, useEffect } from "react";
 import "./Home.css";
 import { CurrentWeather, SearchWeather, Card } from "../../components";
-import { fetchCityWeather, searchWeatherData ,fetchWeatherForecast } from '../../api/apiClient'
+import {
+  fetchCityWeather,
+  searchWeatherData,
+  fetchWeatherForecast,
+} from "../../api/apiClient";
 const Home = () => {
   const [cityWeather, setCityWeather] = useState(null);
   const [latitude, setLatitude] = useState(6.9271);
-  const [longitude , setLongitude ] = useState(79.8612);
+  const [longitude, setLongitude] = useState(79.8612);
+  const [forecastData, setForecastData] = useState(null);
   const handleSearch = ({ latitude, longitude }) => {
-   setLatitude(latitude);
-   setLongitude(longitude);
-   searchWeatherData(latitude, longitude)
-      .then((data) => {
-        setCityWeather(data);
-      });
+    setLatitude(latitude);
+    setLongitude(longitude);
+    searchWeatherData(latitude, longitude).then((data) => {
+      setCityWeather(data);
+    });
+    fetchForecastData(latitude, longitude);
   };
 
-  useEffect(()=>{
-    searchWeatherData(latitude,longitude)
-    .then((data)=>{
+  const fetchForecastData = (latitude, longitude) => {
+    fetchWeatherForecast(latitude, longitude).then((data) => {
+      const today = new Date().toISOString().split("T")[0];
+      const groupedData = data.list.reduce((acc, weatherData) => {
+        const date = weatherData.dt_txt.split(" ")[0];
+
+        if (date > today) {
+          acc[date] = acc[date] || [];
+          acc[date].push(weatherData);
+        }
+
+        return acc;
+      }, {});
+
+      const groupedDataArray = Object.entries(groupedData).map(
+        ([date, data]) => ({
+          date,
+          data,
+        })
+      );
+      setForecastData(groupedDataArray);
+    });
+  };
+  useEffect(() => {
+    searchWeatherData(latitude, longitude).then((data) => {
       setCityWeather(data);
-    })
-  },[])
+    });
+    fetchForecastData(latitude, longitude);
+  }, []);
   return (
     <div className="home-container">
+      <p className="home-title">Weather Wizard</p>
       <div className="current-weather">
-        <CurrentWeather current ={cityWeather} />
+        <CurrentWeather current={cityWeather} />
         <SearchWeather onSearch={handleSearch} />
       </div>
       <div className="forecast-weather">
@@ -34,10 +63,24 @@ const Home = () => {
           <button className="view-more">View More</button>
         </div>
         <div className="forecast-cards">
-          <Card />
-          <Card />
-          <Card />
+          {forecastData ? (
+            forecastData
+              .slice(0, 3)
+              .map((group) => (
+                <Card
+                  key={group.date}
+                  date={group.date}
+                  temp={group.data[0].main.temp}
+                  description={group.data[0].weather[0].description}
+                  humidity={group.data[0].main.humidity}
+                  icon={group.data[0].weather[0].icon}
+                />
+              ))
+          ) : (
+            <p>Loading forecast data...</p>
+          )}
         </div>
+        <hr className="forecast-weather-divider" />
       </div>
     </div>
   );
